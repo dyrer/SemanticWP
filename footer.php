d8ea0ac5f9e3768fbf3e78efbc521fe70d72ea38
Evangelos Athanasiadis <dyrer@hyperwebhost.net>
2014-07-12 22:17:44 +0300

    Page fixed

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 					&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> - All Rights Reserved
 			<?php do_action( 'semantic_ui_wp_theme_credits' ); ?>
 			<a href="http://wordpress.org/" rel="generator"><?php printf( __( '%s', 'semantic-ui-wp-theme' ), 'WordPress' ); ?></a>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'semantic-ui-wp-theme' ), 'semantic-ui-wp-theme', '<a href="http://weibo.com/leopku" rel="designer">leopku</a>@<a href="http://www.himysql.com">HiMySQL</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'semantic-ui-wp-theme' ), 'semantic-ui-wp-theme', '<a href="http://athanasiadis.me" rel="designer">dyrer</a>@<a href="http://www.hyperwebhost.net">Hyper Webhost</a>' ); ?>
 				</div>
 			</div>
 		</div><!-- .site-info -->
@@ -30,15 +30,5 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-6386885-3', 'himysql.com');
-  ga('send', 'pageview');
-
-</script>
 </body>
 </html>
--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="ui twelve column">
+	<div id="primary" class="ui eleven column wide">
 		<main id="main" class="site-main" role="main">
 
 			<?php while ( have_posts() ) : the_post(); ?>
